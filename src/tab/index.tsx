e9fa5736e123e7e2e8ac7bea392fc662108324ba
Alfred-Lau <jian.liu@icloud.com>
2023-12-20 14:32:36 +0800

    feat: dic chore

--- src/tab/index.tsx
+++ src/tab/index.tsx
@@ -1,1 +1,13 @@
-export {};
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+
+import App from './App';
+
+import './App.scss';
+
+ReactDOM.render(
+    <HashRouter>
+        <App />
+    </HashRouter>,
+    document.querySelector('#root'),
+);
